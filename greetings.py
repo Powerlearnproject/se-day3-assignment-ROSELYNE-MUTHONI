5c713e3f0af1097add1bb598d485bee7b5c7baea
ROSELYNE-MUTHONI <rmuthoni317@outlook.com>
2024-08-21 13:48:22 +0300

    created a new repo

---
+++ greetings.py
@@ -0,0 +1,7 @@
+#Define the function
+def greet(person):
+    return f"good morning, {person}!"
+
+#apply
+print(greet("Alice"))
+print(greet("Bob"))
---
+++ navigation.py
@@ -0,0 +1,41 @@
+print("hello Plp Academy")
+#let's store a favour
+# Favourite_Number =73
+# print(f"my Favourite Number is:{Favourite_Number}")
+
+#string
+# greeting = "Hello, PLP Academy"
+# print(greeting)
+
+#Boolean
+# is_python_fun = True
+# is_python_boring = False
+# print(f'is python fun?{is_python_fun}')
+# print(f'is python boring? {is_python_boring}')
+
+#control_flow
+# temperature = 30
+# if temperature > 25:
+#     print('its hot outside! wear shorts.')
+# elif temperature > 15:
+#  print('its warm. maybe wear a light jacket.')
+# else:
+#    print("Brrrr! it's cold. Bundle up!")
+   
+   #module
+   #In-Built Module
+
+   #customized module
+# from math import ulp
+# import greetings
+# print(greetings.greet("mark"))
+# print(greetings.greet("doreen"))
+
+#trial
+weather =cold
+print(weather)
+
+
+
+
+
